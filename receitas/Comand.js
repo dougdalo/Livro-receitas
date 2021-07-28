9a98b8f64ff6d9aba9b88088054d44aefc85993a
dougdalo <dougdalo@gmail.com>
2021-07-27 22:35:18 -0300

    Add arquivo Comand para teste

---
+++ receitas/Comand.js
@@ -0,0 +1,34 @@
+"commit"
+
+"ls -a" mostra itens ocultos
+
+"Iniciar GIT"
+-git init: cria um repositorio
+
+tacket: Arquivo rastreado
+	Unmodified: Arquivo que ainda não foi modificado
+	Modified: Arquivo Unmodified que sofreu modificação
+	Staged: Conceito de chave "oque se passa por trás das cortinas"
+untracked: Arquivo não rastreado
+
+	-cria uma pasta oculta chamada ".git" 	
+"branches  config  description  HEAD  hooks  info  objects  refs"
+	-se faz necessário criar um user e e-mail na config incial do git
+	para e-mail utilizar o comando: 
+	"git config --global user.email "douglas.dalo001@gmail.com""
+	para config user usar o comando:
+	"git config --global user.name dougdalo"
+	
+-git add
+	Move o arquvio para o Staged
+
+-git commit
+	pega os arquvios e move para Unmodified, cria um snapshot do code
+
+
+"Iniciar versionamento"
+"Criar um commit"
+
+-git status
+	mostra o status do repositorio
+	
